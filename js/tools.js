34ed4ec3986fd30ad65c1d85da8d07025133008b
tuenkle <tuenkle1011@gmail.com>
2022-12-25 12:46:00 +0900

    solved cookie undefined problem

--- js/tools.js
+++ js/tools.js
@@ -108,7 +108,7 @@ export function getCookie(name) {
     let matches = document.cookie.match(new RegExp(
         "(?:^|; )" + name.replace(/([\.$?*|{}\(\)\[\]\\\/\+^])/g, '\\$1') + "=([^;]*)"
     ));
-    return matches ? decodeURIComponent(matches[1]) : undefined;
+    return matches ? decodeURIComponent(matches[1]) : "?";
 }
 export function deleteCookie(name) {
     setCookie(name, "", {
